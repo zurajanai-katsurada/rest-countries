11c7c3414a670a0f0b7612d8b9cb0a6d456bca82
zurajanai-katsurada <antusharma6624@gmail.com>
2023-09-12 11:24:29 +0600

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countries from './components/countries/countries'
+import Country from './components/country/country'
+
+function App() {
+
+  return (
+    <>
+      <h1>World tour in 80 days</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/country/country.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './country.css';
+const Country = ({ country }) => {
+    const {name, flags} = country
+    return (
+        
+        <div className="country">
+            <h3>Name: {name?.common}</h3>
+           <img src={flags.png} alt="" />
+        </div>
+    );
+};
+
+export default Country;
